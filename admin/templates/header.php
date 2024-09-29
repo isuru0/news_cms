87b1ab77962a73ce911b6552e5c97db62a1ab56e
isuru0 <isurushehara@gmail.com>
2024-09-29 10:47:40 +0530

    Sytle the sidebar

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="../../styles/templates_header.style.css">
+    <link rel="stylesheet" href="../styles/templates_header.style.css">
 </head>
 <body>
     
-    <div class="dashboard" style="display: flex;">
+    <div class="dashboard">
         <div class="sidebar">
             <h1><a href="./index.php">Dashboard</a></h1>
             <div class="menu-list">
