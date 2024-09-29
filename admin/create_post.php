8656a79a074afe9449d4b4b13fe5835838530795
isuru0 <isurushehara@gmail.com>
2024-09-29 19:54:30 +0530

    Style admin_create_post.php file (+infinite creating post erro)

--- admin/create_post.php
+++ admin/create_post.php
@@ -2,23 +2,24 @@
     include("templates/header.php");
 ?>
 
-    <div class="admin_create_post_post-form">
+    <div class="admin_create_post_post_form">
         <form action="functions.php" method="post" enctype="multipart/form-data">
             <div>
-                <input type="text" name="title" id="" placeholder="Enter title" required>
+                <input type="text" name="title" id="" placeholder="Enter title" class="admin_create_post_post_form_title" required>
             </div>
             <div>
-                <input type="text" name="content" id="" placeholder="Enter content" required>
+                <textarea type="text" name="content" id="" placeholder="Enter content" class="admin_create_post_post_form_content" required></textarea> 
             </div>
-            <div>
-                <input type="file" name="image" id="" accept="image/*" required>
+            <div class="admin_create_post_post_form_image">
+                <p>Add image :</p>
+                <input type="file" name="image" id="" accept="image/*" class="admin_create_post_post_form_image_img" required>
             </div>
             <div>
                 <input type="hidden" name="date" value="<?php echo date("Y/m/d"); ?>">
             </div>
 
-            <div>
-                <input type="submit" value="submit" name="create">
+            <div class="admin_create_post_post_form_btn">
+                <input type="submit" value="Submit" name="create" class="admin_create_post_post_form_btn_input">
             </div>
         </form>
     </div>
