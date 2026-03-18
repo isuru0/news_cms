3aa821a0e52c3ef76ddc080433f98cf312fb20ec
Isuru Shehara <isurushehara@gmail.com>
2026-03-18 12:05:40 +0530

    style: news_post.php and update styles for improved layout and accessibility

--- news_post.php
+++ news_post.php
@@ -9,40 +9,45 @@
 </head>
 <body>
     <div class="background">
-        <div class="home_area">
-
+        <main class="home_area">
             <div class="container" id="scrollbar">
+                <a class="back-link" href="index.php">Back to Headlines</a>
                 <?php
-                    $id = $_GET['id'];
-                    if ($id) {
-                        include("connect.php");
+                    $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
+                    $post = null;
 
+                    if ($id > 0) {
+                        include("connect.php");
                         $sqlSelect = "SELECT * FROM posts WHERE id = $id";
                         $result = mysqli_query($conn, $sqlSelect);
-                        while ($data = mysqli_fetch_array($result)) {
-                            $rawImagePath = ltrim($data["image_path"], "/");
-                            $imageSrc = (strpos($rawImagePath, "admin/") === 0) ? $rawImagePath : "admin/" . $rawImagePath;
+                        $post = mysqli_fetch_assoc($result);
+                    }
+
+                    if ($post) {
+                        $rawImagePath = ltrim($post["image_path"], "/");
+                        $imageSrc = (strpos($rawImagePath, "admin/") === 0) ? $rawImagePath : "admin/" . $rawImagePath;
                 ?>
-                            <div class="post">
-                                <h1><?php echo $data['title']; ?></h1>
-                                <div class="image">
-                                    <img src="<?php echo $imageSrc; ?>" >
-                                </div>
-                                <p class="date"><?php echo $data['date']; ?></p>
-                                <div class="summary_content_box">
-                                    <p class="summary_content"><?php echo $data['content']; ?></p>
-                                </div>
+                <article class="post">
+                    <h1 class="title"><?php echo htmlspecialchars($post['title'], ENT_QUOTES, 'UTF-8'); ?></h1>
+                    <p class="date"><?php echo htmlspecialchars($post['date'], ENT_QUOTES, 'UTF-8'); ?></p>
 
-                            </div>
+                    <div class="image">
+                        <img src="<?php echo $imageSrc; ?>" alt="Post image">
+                    </div>
+
+                    <div class="summary_content_box">
+                        <p class="summary_content"><?php echo nl2br(htmlspecialchars($post['content'], ENT_QUOTES, 'UTF-8')); ?></p>
+                    </div>
+                </article>
                 <?php
-                        }
-
-                    }else {
-                        echo "No Post Found";
+                    } else {
+                ?>
+                <div class="empty-state">No Post Found</div>
+                <?php
                     }
                 ?>
             </div>
-        </div>
+        </main>
     </div>
 </body>
 </html>
